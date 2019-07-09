6eb83f0d3781e1c5bb2fe27b08982096eb03a4a0
Stéphane Duchesneau <stephane.duchesneau@eoscanada.com>
2019-07-09 14:27:40 -0400

    use lock and counter to allow dynamic sizing, add tests

--- group.go
+++ group.go
@@ -2,33 +2,58 @@ package llerrgroup
 
 import (
 	"sync"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
 type Group struct {
 	errgroup.Group
-	queue     chan bool
-	quit      chan struct{}
-	closeLock sync.Mutex
+	threadsLock    sync.Mutex
+	runningThreads int
+	targetThreads  int
+	threadFreed    chan bool
+	quit           chan struct{}
+	closeLock      sync.Mutex
 
 	callsCount int
 }
 
+func (g *Group) SetSize(parallelOperations int) {
+	g.threadsLock.Lock()
+	defer g.threadsLock.Unlock()
+	g.targetThreads = parallelOperations
+}
+
 func New(parallelOperations int) *Group {
 	return &Group{
-		queue: make(chan bool, parallelOperations),
-		quit:  make(chan struct{}, 0),
+		targetThreads: parallelOperations,
+		quit:          make(chan struct{}, 0),
+		threadFreed:   make(chan bool, 1),
 	}
 }
 
 // Stop blocks for a free queue position, and returns whether you should stop processing requests.  In a for loop
 func (g *Group) Stop() bool {
-	select {
-	case g.queue <- true:
-		return false
-	case <-g.quit:
-		return true
+
+	for {
+		select {
+		case <-g.quit:
+			return true
+		default:
+		}
+
+		g.threadsLock.Lock()
+		if g.runningThreads < g.targetThreads {
+			g.runningThreads += 1
+			g.threadsLock.Unlock()
+			return false
+		}
+		g.threadsLock.Unlock()
+		select {
+		case <-time.After(10 * time.Millisecond):
+		case <-g.threadFreed:
+		}
 	}
 }
 func (g *Group) CallsCount() int {
@@ -51,7 +76,13 @@ func (g *Group) Go(f func() error) {
 		}
 		g.closeLock.Unlock()
 
-		<-g.queue
+		g.threadsLock.Lock()
+		g.runningThreads -= 1
+		if len(g.threadFreed) == 0 {
+			g.threadFreed <- true
+		}
+		g.threadsLock.Unlock()
+
 		return err
 	})
 }
--- group_test.go
+++ group_test.go
@@ -1,9 +1,128 @@
 package llerrgroup
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/atomic"
 )
+
+func Test_ParallelRuns(t *testing.T) {
+	var cnt atomic.Uint32
+	g := New(8)
+	for i := 0; i < 12; i++ {
+		if stop := g.Stop(); stop != false {
+			t.Fail()
+		}
+
+		g.Go(func() error {
+			cnt.Add(1)
+			time.Sleep(time.Millisecond * 10)
+			return nil
+		})
+
+	}
+	errCh := make(chan error, 1)
+	go func() {
+		err := g.Wait()
+		errCh <- err
+	}()
+	select {
+	case <-time.After(time.Millisecond * 30):
+		t.Fail()
+	case err := <-errCh:
+		require.NoError(t, err)
+	}
+
+	assert.Equal(t, uint32(12), cnt.Load())
+	assert.Equal(t, 12, g.CallsCount())
+
+}
+
+func Test_ParallelLimits(t *testing.T) {
+	var cnt atomic.Uint32
+	g := New(1)
+	for i := 0; i < 100; i++ {
+		if stop := g.Stop(); stop != false {
+			fmt.Println("failing on stop")
+			t.Fail()
+		}
+		assert.Equal(t, i, int(cnt.Load()))
+		g.Go(func() error {
+			cnt.Add(1)
+			return nil
+		})
+	}
+	err := g.Wait()
+	assert.NoError(t, err)
+
+}
+
+func Test_DynamicParallelReduction(t *testing.T) {
+	var cnt atomic.Uint32
+	parallelRunningIndication := false
+	g := New(10)
+	for i := 0; i < 100; i++ {
+		if stop := g.Stop(); stop != false {
+			fmt.Println("failing on stop")
+			t.Fail()
+		}
+		if i == 19 {
+			g.SetSize(1)
+		}
+		if i < 20 {
+			if i != int(cnt.Load()) {
+				parallelRunningIndication = true
+			}
+		} else {
+			assert.Equal(t, i, int(cnt.Load()), "routines after #20 should be run one at a time")
+		}
+		g.Go(func() error {
+			cnt.Add(1)
+			return nil
+		})
+	}
+	err := g.Wait()
+	assert.NoError(t, err)
+
+	assert.True(t, parallelRunningIndication, "cannot validate that first 20 routines run in parallel")
+}
+
+func Test_DynamicParallelIncrease(t *testing.T) {
+	var cnt atomic.Uint32
+	parallelRunningIndication := false
+	g := New(1)
+	for i := 0; i < 100; i++ {
+		if stop := g.Stop(); stop != false {
+			fmt.Println("failing on stop")
+			t.Fail()
+		}
+		if i == 20 {
+			g.SetSize(10)
+		}
+		if i > 20 {
+			if i != int(cnt.Load()) {
+				parallelRunningIndication = true
+			}
+		} else {
+			assert.Equal(t, i, int(cnt.Load()), "routines before #20 should be run one at a time")
+		}
+		g.Go(func() error {
+			cnt.Add(1)
+			return nil
+		})
+	}
+	err := g.Wait()
+	assert.NoError(t, err)
+
+	assert.True(t, parallelRunningIndication, "cannot validate that routines after #20 run in parallel")
+
+}
 
 func ExampleGroupParallelAndQuitSoon() {
 	url := []string{"http://1", "http://2"}
